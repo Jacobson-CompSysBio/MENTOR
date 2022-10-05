1ba16d2aeff7abe4ecd66a417a6d044787b11266
izaakm <1505395+izaakm@users.noreply.github.com>
2022-10-05 08:14:05 -0400

    Update version in setup.py, version.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="functional_partitioning",
-    version="0.0.1",
+    version="0.1.0",
     author='J. I. Miller',
     author_email='millerji@ornl.gov',
     description='Functional partitioning.',
