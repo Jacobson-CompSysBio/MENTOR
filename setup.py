b1607956fa7ff58340a3823e746899574396a736
izaakm <1505395+izaakm@users.noreply.github.com>
2022-10-05 08:17:44 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="functional_partitioning",
-    version="0.1.0",
+    version="0.1.1",
     author='J. I. Miller',
     author_email='millerji@ornl.gov',
     description='Functional partitioning.',
