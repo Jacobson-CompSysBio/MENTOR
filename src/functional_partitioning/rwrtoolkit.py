1764c01f2499ad07ef72bf2ba93981dcf77e9328
izaakm <1505395+izaakm@users.noreply.github.com>
2023-01-04 06:51:48 -0500

    Initialize rwrtoolkit module

---
+++ src/functional_partitioning/rwrtoolkit.py
@@ -0,0 +1,35 @@
+'''
+Wrapper functions for RWRtoolkit
+'''
+
+def conda_activate():
+    '''
+    Return the command to activate conda environment.
+    '''
+    ...
+
+def rwr_singletons():
+    '''
+    Return the command to run RWR-singletons.
+    '''
+    ...
+
+def run():
+    '''
+    Run the whole pipeline in a subprocess.
+    '''
+    ...
+
+def compress_results():
+    '''
+    Gzip the RWR results.
+    '''
+    ...
+
+def fullranks_to_matrix():
+    '''
+    Convert RWR "fullranks" file to matrix.
+    '''
+    ...
+
+# END
