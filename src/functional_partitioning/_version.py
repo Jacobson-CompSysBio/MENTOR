d36a91409fa1428af579258bf5fe0147445fc82c
izaakm <1505395+izaakm@users.noreply.github.com>
2023-03-09 12:49:07 -0500

    Change: Use __version__ as string instead of get_version

--- src/functional_partitioning/_version.py
+++ src/functional_partitioning/_version.py
@@ -1,17 +1,1 @@
-# # Error during installation (pip install -e .):
-# # AttributeError: module 'functional_partitioning' has no attribute '__version__'. Did you mean: '_version'?
-# def get_version():
-#     import os
-#     import subprocess
-#     cwd = os.path.getcwd()
-#     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-#     version = subprocess.check_output(
-#         'git describe --tags --dirty --always'.split()
-#     ).decode().strip()
-#     os.chdir(cwd)
-#     return version
-
-# def get_version():
-#     return 'v0.5.1'
-
 __version__ = 'v0.5.1'
