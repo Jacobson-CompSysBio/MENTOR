12cbfe97f00c5e49f63425fb6b62f0aa75b71697
izaakm <1505395+izaakm@users.noreply.github.com>
2023-03-23 07:01:12 -0400

    Add tests for --init-test-fullranks

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,6 +5,7 @@ import pandas as pd
 import os
 import unittest
 import tempfile
+import pathlib
 
 from numpy import testing
 from sklearn import datasets, preprocessing
@@ -30,12 +31,16 @@ def test_entrypoint():
     assert exit_status == 0
 
 
-class TestInitTestFullranks(unittest.TestCase):
+class TestInitFullranks(unittest.TestCase):
     def setUp(self):
-        pass
+        self.path_to_fullranks = pathlib.Path(tempfile.mkdtemp()) / 'fullranks.tsv'
+        self.exit_status = os.system(f'functional_partitioning --init-test-fullranks {self.path_to_fullranks} --no-partition')
 
-    def hash_thing(self):
-        pass
+    def test_exit_status(self):
+        assert self.exit_status == 0
 
-    def test_thing(self):
-        pass
+    def test_fullranks_exists(self):
+        assert self.path_to_fullranks.exists()
+
+
+# END.
