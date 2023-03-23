54c79f4dc42de7d18d5d101bc42597df449915a4
izaakm <1505395+izaakm@users.noreply.github.com>
2023-03-23 08:51:04 -0400

    Test functional_partitioning

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -20,27 +20,85 @@ from functional_partitioning import _plot as plot
 from scipy.spatial import distance
 from scipy.cluster import hierarchy
 
+# Fixtures.
+def _initialize_test_fullranks():
+    path_to_fullranks = pathlib.Path(tempfile.mkdtemp()) / 'fullranks.tsv'
+    os.system(f'functional_partitioning --init-test-fullranks {path_to_fullranks} --no-partition')
+    return path_to_fullranks
+
+def _initialize_outdir():
+    outdir = pathlib.Path(tempfile.mkdtemp())
+    return outdir
+
 
 # Test entry point
 def test_entrypoint():
     exit_status = os.system('functional_partitioning --help')
     assert exit_status == 0
 
-def test_entrypoint():
+def test_version():
     exit_status = os.system('functional_partitioning --version')
     assert exit_status == 0
 
+# Test that the fullranks file is created.
+def test_init_fullranks_exit():
+    path_to_fullranks = pathlib.Path(tempfile.mkdtemp()) / 'fullranks.tsv'
+    exit_status = os.system(f'functional_partitioning --init-test-fullranks {path_to_fullranks} --no-partition')
+    assert exit_status == 0
+
+def test_init_fullranks_exists():
+    path_to_fullranks = pathlib.Path(tempfile.mkdtemp()) / 'fullranks.tsv'
+    os.system(f'functional_partitioning --init-test-fullranks {path_to_fullranks} --no-partition')
+    assert path_to_fullranks.exists()
 
 class TestInitFullranks(unittest.TestCase):
-    def setUp(self):
-        self.path_to_fullranks = pathlib.Path(tempfile.mkdtemp()) / 'fullranks.tsv'
-        self.exit_status = os.system(f'functional_partitioning --init-test-fullranks {self.path_to_fullranks} --no-partition')
+    '''Basic example of how to use setUpClass and tearDownClass'''
+    @classmethod
+    def setUpClass(cls):
+        cls._path_to_fullranks = _initialize_test_fullranks()
 
-    def test_exit_status(self):
-        assert self.exit_status == 0
+    @classmethod
+    def tearDownClass(cls):
+        os.remove(cls._path_to_fullranks)
+
+    def test_foo(self):
+        assert os.path.exists(self._path_to_fullranks)
+
+# Test the CLI.
+class TestFunctionalPartitioning(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls._path_to_fullranks = _initialize_test_fullranks()
+        cls._outdir = _initialize_outdir()
+        cls._exit_code = os.system(f'functional_partitioning --rwr-fullranks {cls._path_to_fullranks} --outdir {cls._outdir}')
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove(cls._path_to_fullranks)
+
+    def test_exit_code(self):
+        assert self._exit_code == 0
 
     def test_fullranks_exists(self):
-        assert self.path_to_fullranks.exists()
+        assert self._path_to_fullranks.exists()
+
+    def test_outdir_exists(self):
+        assert self._outdir.exists()
+
+    def test_clusters_exists(self):
+        assert (self._outdir / 'clusters.tsv').exists()
+
+    def test_dendrogram_exists(self):
+        assert (self._outdir / 'dendrogram.png').exists()
+
+    def test_dissimilarity_matrix_exists(self):
+        assert (self._outdir / 'dissimilarity-matrix.tsv').exists()
+
+    def test_dissimilarity_stats_exists(self):
+        assert (self._outdir / 'dissimilarity-stats.tsv').exists()
+
+    def test_plot_exists(self):
+        assert (self._outdir / 'distribution-of-pairwise-dissimilarities.png').exists()
 
 
 # END.
