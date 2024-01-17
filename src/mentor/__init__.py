220a6e31145a31983ed6afff8c24b873d7bb6bb7
ayeTown <al1c3@outlook.com>
2024-01-16 19:21:42 -0500

    changing fp to mentor

---
+++ src/mentor/__init__.py
@@ -0,0 +1,10 @@
+from functional_partitioning._version import __version__
+from functional_partitioning import _cluster as cluster
+from functional_partitioning import _metrics as metrics
+
+# __version__ = get_version()
+__all__ = [
+    '__version__',
+    'cluster',
+    'metrics'
+]
