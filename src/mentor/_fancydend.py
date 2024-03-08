72e1e01a3eba2d745b6f8d69bac74ddd814f5a48
ayeTown <al1c3@outlook.com>
2024-03-08 13:35:40 -0500

    remove pcutoff option

--- src/mentor/_fancydend.py
+++ src/mentor/_fancydend.py
@@ -24,7 +24,7 @@ def fancy_dendrogram(
         maxsize,
         heatmaps,
         reordercols,
-        pcutoff,
+        #pcutoff,
         squish,
         relwidths,
         plotwidth,
@@ -54,7 +54,6 @@ def fancy_dendrogram(
         command += f' --heatmaps={heatmaps}'
     if reordercols is True:
         command += f' --reordercols'
-# note: need to add pcutoff option as well
     if squish is not None:
         command += f' --squish={squish}'
     if relwidths is not None:
